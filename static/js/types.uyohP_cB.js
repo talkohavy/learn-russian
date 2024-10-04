3d21231807c76dc600cc096cb56c711c3e95ee54
Tal Kohavy <talkohavy@gmail.com>
2024-10-04 15:17:36 +0300

    RELEASE-1

---
+++ static/js/types.uyohP_cB.js
@@ -0,0 +1,2 @@
+var n=(s=>(s.Animals="animals",s.Jobs="jobs",s.Food="food",s.Colors="colors",s.BodyParts="body-parts",s.BodyRelation="body-relation",s.MusicalInstrument="musical-instrument",s.Verbs="verbs",s.Belonging="belonging",s.Family="family",s.QuestionWord="question-word",s.Countries="countries",s.Greetings="greetings",s.Other="other",s))(n||{});export{n as C};
+//# sourceMappingURL=types.iVLtlLpY.js.map
