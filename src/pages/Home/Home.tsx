22cb499885cf93a36d149e23ecc9b3f10335a055
Tal Kohavy <talkohavy@gmail.com>
2024-10-05 13:56:46 +0300

    HOME - remove console.log

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,17 @@
+import { useMemo } from 'react';
 import Card from '@src/components/Card';
 import { Category } from '@src/utils/types';
-import { useMemo } from 'react';
 
 export default function HomePage() {
   const categories = useMemo(() => {
     const keys = Object.keys(Category);
-
-    keys.forEach((key, index) => {
-      console.log(`${key} has index ${index}`);
-    });
 
     return keys;
   }, []);
 
   return (
     <div className='size-full p-6'>
-      <div className='flex flex-wrap gap-3 justify-start'>
+      <div className='flex flex-wrap justify-start gap-3'>
         {categories.map((categoryName) => (
           <Card key={categoryName} title={categoryName} />
         ))}
