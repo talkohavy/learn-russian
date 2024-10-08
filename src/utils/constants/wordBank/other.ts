988cdb6ac12885b123432fe62e5e03039bec0d1c
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:48:11 +0300

    converted отчер words to the new form

--- src/utils/constants/wordBank/other.ts
+++ src/utils/constants/wordBank/other.ts
@@ -1,591 +1,566 @@
-import { Category } from '@src/utils/types';
+import { Category, type Word } from '@src/utils/types';
 
-export const other = [
+export const other: Array<Word> = [
   {
-    main: {
-      spelling: 'друг',
-      soundsLike: 'DROOG',
-      meaning: ['friend'],
-    },
+    spelling: 'друг',
+    soundsLike: 'DROOG',
+    meaning: ['friend'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'здесь',
-      soundsLike: 'ZDIS',
-      meaning: ['here'],
-    },
+    spelling: 'здесь',
+    soundsLike: 'ZDIS',
+    meaning: ['here'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'там',
-      soundsLike: 'TAM',
-      meaning: ['there'],
-    },
+    spelling: 'там',
+    soundsLike: 'TAM',
+    meaning: ['there'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'это',
-      soundsLike: 'ETA', // ETAT ETI
-      meaning: ['this'],
-      // metadata: ['етот', 'эти']
-    },
+    spelling: 'это',
+    soundsLike: 'ETA', // ETAT ETI
+    meaning: ['this'],
+    // metadata: ['етот', 'эти']
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'тарелка',
-      soundsLike: 'TAREILKA',
-      meaning: ['plate'],
-    },
-    plural: {
-      spelling: 'тарелки',
-      soundsLike: 'TAREILKI',
-      meaning: ['plates'],
-    },
+    spelling: 'тарелка',
+    soundsLike: 'TAREILKA',
+    meaning: ['plate'],
+
+    pluralForeignKey: 'тарелки',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'мальчик',
-      soundsLike: '**MAL**CHIK',
-      meaning: ['boy'],
-    },
-    plural: {
-      spelling: 'мальчики',
-      soundsLike: '**MAL**CHIKI',
-      meaning: ['boys'],
-    },
+    spelling: 'тарелки',
+    soundsLike: 'TAREILKI',
+    meaning: ['plates'],
+    singularForeignKey: 'тарелка',
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'девочка',
-      soundsLike: '**DYE**VOCHKA',
-      meaning: ['girl'],
-    },
-    plural: {
-      spelling: 'девочки',
-      soundsLike: '**DYE**VOCHKI',
-      meaning: ['girls'],
-    },
+    spelling: 'мальчик',
+    soundsLike: '**MAL**CHIK',
+    meaning: ['boy'],
+    pluralForeignKey: 'мальчики',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'дети',
-      soundsLike: '**DEY**TI',
-      meaning: ['children'],
-    },
+    spelling: 'мальчики',
+    soundsLike: '**MAL**CHIKI',
+    meaning: ['boys'],
+    singularForeignKey: 'мальчик',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'мужчина',
-      soundsLike: "MUI**ZHCH'I**NA",
-      meaning: ['man'],
-    },
-    plural: {
-      spelling: 'мужчины',
-      soundsLike: 'MUI**SHI**NI',
-      meaning: ['men'],
-    },
+    spelling: 'девочка',
+    soundsLike: '**DYE**VOCHKA',
+    meaning: ['girl'],
+    pluralForeignKey: 'девочки',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'женщинa',
-      soundsLike: '**ZHEN**SHINA',
-      meaning: ['woman'],
-    },
-    plural: {
-      spelling: 'женщины',
-      soundsLike: '**ZHEN**SHINIE',
-      meaning: ['women'],
-    },
+    spelling: 'девочки',
+    soundsLike: '**DYE**VOCHKI',
+    meaning: ['girls'],
+    singularForeignKey: 'девочка',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'дом',
-      soundsLike: 'DOM',
-      meaning: ['house'],
-    },
+    spelling: 'дети',
+    soundsLike: '**DEY**TI',
+    meaning: ['children'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'офис',
-      soundsLike: '**O**FIS',
-      meaning: ['office'],
-    },
-    plural: {
-      spelling: 'офисы',
-      soundsLike: '**O**FISI',
-      meaning: ['offices'],
-    },
+    spelling: 'мужчина',
+    soundsLike: "MUI**ZHCH'I**NA",
+    meaning: ['man'],
+    pluralForeignKey: 'мужчины',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'метро',
-      soundsLike: 'MET**RO**',
-      meaning: ['subway'],
-    },
+    spelling: 'мужчины',
+    soundsLike: 'MUI**SHI**NI',
+    meaning: ['men'],
+    singularForeignKey: 'мужчина',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'человек',
-      soundsLike: 'CHELA**VEK**',
-      meaning: ['person'],
-    },
+    spelling: 'женщина',
+    soundsLike: '**ZHEN**SHINA',
+    meaning: ['woman'],
+
+    pluralForeignKey: 'женщины',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'озеро',
-      soundsLike: '**O**ZIRA',
-      meaning: ['lake'],
-    },
-    plural: {
-      spelling: 'озёра',
-      soundsLike: 'AZ**YO**RA',
-      meaning: ['lakes'],
-    },
+    spelling: 'женщины',
+    soundsLike: '**ZHEN**SHINIE',
+    meaning: ['women'],
+    singularForeignKey: 'женщина',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'тетрадь',
-      soundsLike: 'TET**RAD**',
-      meaning: ['notebook'],
-    },
+    spelling: 'дом',
+    soundsLike: 'DOM',
+    meaning: ['house'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'стол',
-      soundsLike: 'STOL',
-      meaning: ['table'],
-    },
-    plural: {
-      spelling: 'столы',
-      soundsLike: 'STA**LUI**',
-      meaning: ['tables'],
-    },
+    spelling: 'офис',
+    soundsLike: '**O**FIS',
+    meaning: ['office'],
+
+    pluralForeignKey: 'офисы',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'полотенце',
-      soundsLike: 'PALA**TIEN**TSYE',
-      meaning: ['towel'],
-    },
-    plural: {
-      spelling: 'полотенца',
-      soundsLike: 'PALA**TIEN**TSA',
-      meaning: ['towels'],
-    },
+    spelling: 'офисы',
+    soundsLike: '**O**FISI',
+    meaning: ['offices'],
+    singularForeignKey: 'офис',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'телефон',
-      soundsLike: 'TYELEPHON',
-      meaning: ['phone'],
-    },
+    spelling: 'метро',
+    soundsLike: 'MET**RO**',
+    meaning: ['subway'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'такси',
-      soundsLike: 'TAKSI',
-      meaning: ['taxi'],
-    },
+    spelling: 'человек',
+    soundsLike: 'CHELA**VEK**',
+    meaning: ['person'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'план',
-      soundsLike: 'PLAN',
-      meaning: ['plan'],
-    },
+    spelling: 'озеро',
+    soundsLike: '**O**ZIRA',
+    meaning: ['lake'],
+    pluralForeignKey: 'озёра',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'ребёнок',
-      soundsLike: 'REB**YO**NAK',
-      meaning: ['child', 'baby'],
-    },
+    spelling: 'озёра',
+    soundsLike: 'AZ**YO**RA',
+    meaning: ['lakes'],
+    singularForeignKey: 'озеро',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'велосипед',
-      soundsLike: 'VELOSIP**YED**',
-      meaning: ['bicycle'],
-    },
+    spelling: 'тетрадь',
+    soundsLike: 'TET**RAD**',
+    meaning: ['notebook'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'ну',
-      soundsLike: 'NU',
-      meaning: ['well'],
-    },
+    spelling: 'стол',
+    soundsLike: 'STOL',
+    meaning: ['table'],
+    pluralForeignKey: 'столы',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'всё',
-      soundsLike: 'VSO',
-      meaning: ['everything', 'all', 'anything'],
-    },
+    spelling: 'столы',
+    soundsLike: 'STA**LUI**',
+    meaning: ['tables'],
+    singularForeignKey: 'стол',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'уже',
-      soundsLike: 'UZHE',
-      meaning: ['already'],
-    },
+    spelling: 'полотенце',
+    soundsLike: 'PALA**TIEN**TSYE',
+    meaning: ['towel'],
+    pluralForeignKey: 'полотенца',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'конечно',
-      soundsLike: 'KANI**YECH**NA',
-      meaning: ['of course'],
-    },
+    spelling: 'полотенца',
+    soundsLike: 'PALA**TIEN**TSA',
+    meaning: ['towels'],
+    singularForeignKey: 'полотенце',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'утро',
-      soundsLike: 'UTRA',
-      meaning: ['morning'],
-    },
+    spelling: 'телефон',
+    soundsLike: 'TYELEPHON',
+    meaning: ['phone'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'кафе',
-      soundsLike: 'KAFE',
-      meaning: ['cafe'],
-    },
+    spelling: 'такси',
+    soundsLike: 'TAKSI',
+    meaning: ['taxi'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'план',
+    soundsLike: 'PLAN',
+    meaning: ['plan'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'ребёнок',
+    soundsLike: 'REB**YO**NAK',
+    meaning: ['child', 'baby'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'велосипед',
+    soundsLike: 'VELOSIP**YED**',
+    meaning: ['bicycle'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'ну',
+    soundsLike: 'NU',
+    meaning: ['well'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'всё',
+    soundsLike: 'VSO',
+    meaning: ['everything', 'all', 'anything'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'уже',
+    soundsLike: 'UZHE',
+    meaning: ['already'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'конечно',
+    soundsLike: 'KANI**YECH**NA',
+    meaning: ['of course'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'утро',
+    soundsLike: 'UTRA',
+    meaning: ['morning'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'кафе',
+    soundsLike: 'KAFE',
+    meaning: ['cafe'],
+
     points: 0,
     categories: [Category.Other],
   },
 
   {
-    main: {
-      spelling: 'парк',
-      soundsLike: 'PARK',
-      meaning: ['park'],
-    },
+    spelling: 'парк',
+    soundsLike: 'PARK',
+    meaning: ['park'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'добрый',
-      soundsLike: 'DOBREY',
-      meaning: ['good', 'kind', 'nice'],
-    },
+    spelling: 'добрый',
+    soundsLike: 'DOBREY',
+    meaning: ['good', 'kind', 'nice'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'дела',
-      soundsLike: 'DYELA',
-      meaning: ['matter', 'business', 'case'],
-    },
+    spelling: 'дела',
+    soundsLike: 'DYELA',
+    meaning: ['matter', 'business', 'case'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'день',
-      soundsLike: 'DYЕN',
-      meaning: ['day'],
-    },
+    spelling: 'день',
+    soundsLike: 'DYЕN',
+    meaning: ['day'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'вечер',
-      soundsLike: '**VYE**CHER',
-      meaning: ['evening'],
-    },
+    spelling: 'вечер',
+    soundsLike: '**VYE**CHER',
+    meaning: ['evening'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'город',
-      soundsLike: '**GO**RAD',
-      meaning: ['city'],
-    },
+    spelling: 'город',
+    soundsLike: '**GO**RAD',
+    meaning: ['city'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'свитер',
-      soundsLike: '**SVI**TER',
-      meaning: ['Sweater'],
-    },
+    spelling: 'свитер',
+    soundsLike: '**SVI**TER',
+    meaning: ['Sweater'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'или',
-      soundsLike: '**I**LI',
-      meaning: ['or'],
-    },
+    spelling: 'или',
+    soundsLike: '**I**LI',
+    meaning: ['or'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'рюкзак',
-      soundsLike: '**RIU**KZAK',
-      meaning: ['backpack'],
-    },
+    spelling: 'рюкзак',
+    soundsLike: '**RIU**KZAK',
+    meaning: ['backpack'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'да',
-      soundsLike: 'DA',
-      meaning: ['yes'],
-    },
+    spelling: 'да',
+    soundsLike: 'DA',
+    meaning: ['yes'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'нет',
-      soundsLike: 'NYET',
-      meaning: ['no'],
-    },
+    spelling: 'нет',
+    soundsLike: 'NYET',
+    meaning: ['no'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'меню',
-      soundsLike: 'MENU',
-      meaning: ['menu'],
-    },
+    spelling: 'меню',
+    soundsLike: 'MENU',
+    meaning: ['menu'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'сон',
-      soundsLike: 'SON',
-      meaning: ['dream'],
-    },
+    spelling: 'сон',
+    soundsLike: 'SON',
+    meaning: ['dream'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'маленький',
-      soundsLike: 'MALYENKI',
-      meaning: ['small'],
-    },
+    spelling: 'маленький',
+    soundsLike: 'MALYENKI',
+    meaning: ['small'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'жаль',
-      soundsLike: 'ZHAL',
-      meaning: ['what a pity', 'it is a pitty', 'sorry'],
-    },
+    spelling: 'жаль',
+    soundsLike: 'ZHAL',
+    meaning: ['what a pity', 'it is a pitty', 'sorry'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'йога',
-      soundsLike: 'YOGA',
-      meaning: ['yoga'],
-    },
+    spelling: 'йога',
+    soundsLike: 'YOGA',
+    meaning: ['yoga'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'шорты',
-      soundsLike: 'SHORTI',
-      meaning: ['shorts'],
-    },
+    spelling: 'шорты',
+    soundsLike: 'SHORTI',
+    meaning: ['shorts'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'кровать',
-      soundsLike: 'KRA**VAT**',
-      meaning: ['bed'],
-    },
+    spelling: 'кровать',
+    soundsLike: 'KRA**VAT**',
+    meaning: ['bed'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'тоже',
-      soundsLike: '**TO**ZHE',
-      meaning: ['also', 'too'],
-    },
+    spelling: 'тоже',
+    soundsLike: '**TO**ZHE',
+    meaning: ['also', 'too'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'туалет',
-      soundsLike: '**TU**ALET',
-      meaning: ['toilet', 'bathroom'],
-    },
+    spelling: 'туалет',
+    soundsLike: '**TU**ALET',
+    meaning: ['toilet', 'bathroom'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'камень',
-      soundsLike: '**KA**MYEN',
-      meaning: ['stone'],
-    },
+    spelling: 'камень',
+    soundsLike: '**KA**MYEN',
+    meaning: ['stone'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'дверь',
-      soundsLike: 'DVIR',
-      meaning: ['door'],
-    },
-    plural: {
-      spelling: 'двери',
-      soundsLike: 'DVIRI',
-      meaning: ['doors'],
-    },
+    spelling: 'дверь',
+    soundsLike: 'DVIR',
+    meaning: ['door'],
+    pluralForeignKey: 'двери',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'карандаш',
-      soundsLike: 'KARAN**DASH**',
-      meaning: ['pencil'],
-    },
+    spelling: 'двери',
+    soundsLike: 'DVIRI',
+    meaning: ['doors'],
+    singularForeignKey: 'дверь',
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'дерево',
-      soundsLike: 'DYEREVA',
-      meaning: ['tree'],
-    },
+    spelling: 'карандаш',
+    soundsLike: 'KARAN**DASH**',
+    meaning: ['pencil'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'далеко',
-      soundsLike: 'DALIE**KO**',
-      meaning: ['far away'],
-    },
+    spelling: 'дерево',
+    soundsLike: 'DYEREVA',
+    meaning: ['tree'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'площадь',
-      soundsLike: '**PLO**SHID',
-      meaning: ['square (place)'],
-    },
+    spelling: 'далеко',
+    soundsLike: 'DALIE**KO**',
+    meaning: ['far away'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'на',
-      soundsLike: 'NA',
-      meaning: ['on', 'is on', 'there is...on'],
-    },
+    spelling: 'площадь',
+    soundsLike: '**PLO**SHID',
+    meaning: ['square (place)'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'страшно',
-      soundsLike: '**STRASH**NA',
-      meaning: ['scary'],
-    },
+    spelling: 'на',
+    soundsLike: 'NA',
+    meaning: ['on', 'is on', 'there is...on'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'акно',
-      soundsLike: 'AK**NO**',
-      meaning: ['window'],
-    },
+    spelling: 'страшно',
+    soundsLike: '**STRASH**NA',
+    meaning: ['scary'],
+
     points: 0,
     categories: [Category.Other],
   },
   {
-    main: {
-      spelling: 'стул',
-      soundsLike: 'STOOL',
-      meaning: ['chair'],
-    },
+    spelling: 'акно',
+    soundsLike: 'AK**NO**',
+    meaning: ['window'],
+
+    points: 0,
+    categories: [Category.Other],
+  },
+  {
+    spelling: 'стул',
+    soundsLike: 'STOOL',
+    meaning: ['chair'],
+
     points: 0,
     categories: [Category.Other],
   },
   // {
-  //   main: {
+  //
   //     spelling: '',
   //     soundsLike: '',
   //     meaning: [''],
