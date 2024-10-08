6478fdd4db72c2ff19227b0d0542e635c3a6d6c1
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:27:48 +0300

    converted countries words to the new form

--- src/utils/constants/wordBank/countries.ts
+++ src/utils/constants/wordBank/countries.ts
@@ -2,65 +2,51 @@ import { Category, type Word } from '@src/utils/types';
 
 export const countries: Array<Word> = [
   {
-    main: {
-      spelling: 'америка',
-      soundsLike: 'A**ME**RIKA',
-      meaning: ['America'],
-    },
+    spelling: 'америка',
+    soundsLike: 'A**ME**RIKA',
+    meaning: ['America'],
     points: 0,
     categories: [Category.Countries],
   },
   {
-    main: {
-      spelling: 'бразилия',
-      soundsLike: 'BRA**ZIL**IA',
-      meaning: ['Brazil'],
-    },
+    spelling: 'бразилия',
+    soundsLike: 'BRA**ZIL**IA',
+    meaning: ['Brazil'],
     points: 0,
     categories: [Category.Countries],
   },
   {
-    main: {
-      spelling: 'канада',
-      soundsLike: 'KA**NA**DA',
-      meaning: ['Canada'],
-    },
+    spelling: 'канада',
+    soundsLike: 'KA**NA**DA',
+    meaning: ['Canada'],
     points: 0,
     categories: [Category.Countries],
   },
   {
-    main: {
-      spelling: 'россия',
-      soundsLike: 'RA**SIA**',
-      meaning: ['Russia'],
-    },
+    spelling: 'россия',
+    soundsLike: 'RA**SIA**',
+    meaning: ['Russia'],
     points: 0,
     categories: [Category.Countries],
   },
   {
-    main: {
-      spelling: 'италия',
-      soundsLike: 'I**TA**LIYA',
-      meaning: ['Italy'],
-    },
+    spelling: 'италия',
+    soundsLike: 'I**TA**LIYA',
+    meaning: ['Italy'],
     points: 0,
     categories: [Category.Countries],
   },
   {
-    main: {
-      spelling: 'монголия',
-      soundsLike: 'MAN**GO**LIYA',
-      meaning: ['Mongolia'],
-    },
+    spelling: 'монголия',
+    soundsLike: 'MAN**GO**LIYA',
+    meaning: ['Mongolia'],
     points: 0,
     categories: [Category.Countries],
   },
   {
-    main: {
-      spelling: 'украина',
-      soundsLike: 'UKRA**INA**',
-      meaning: ['Ukraine'],
-    },
+    spelling: 'украина',
+    soundsLike: 'UKRA**INA**',
+    meaning: ['Ukraine'],
     points: 0,
     categories: [Category.Countries],
   },
