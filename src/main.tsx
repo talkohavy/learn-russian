9d75a65f746aa4345eef46a1a794b19de328c1b3
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 09:53:35 +0300

    initializing indexDBClient inside main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,31 @@
 import { StrictMode } from 'react';
-import axios from 'axios';
 import { createRoot } from 'react-dom/client';
-import { Provider as StoreProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@src/App';
-import { initDAL } from './DAL';
+import SuspenseUntilReady from './components/SuspenseUntilReady';
+import { IndexedDBClient } from './indexedDBClient';
 import DarkThemeProvider from './providers/DarkThemeProvider';
-import { createStore } from './store';
+import { dbName, tableName } from './utils/constants';
 import './index.css';
 
-const httpClientAxios = axios.create({ baseURL: 'http://localhost:8000', withCredentials: true });
-
-const store = createStore({ preloadedState: {} });
-
-initDAL({ httpClient: httpClientAxios });
+export var indexDBClient: IndexedDBClient;
 
 function Client() {
   return (
     <StrictMode>
-      <StoreProvider store={store}>
+      <SuspenseUntilReady
+        asyncFn={async () => {
+          indexDBClient = new IndexedDBClient(dbName, tableName);
+
+          await indexDBClient.init();
+        }}
+      >
         <BrowserRouter>
           <DarkThemeProvider>
             <App />
           </DarkThemeProvider>
         </BrowserRouter>
-      </StoreProvider>
+      </SuspenseUntilReady>
     </StrictMode>
   );
 }
