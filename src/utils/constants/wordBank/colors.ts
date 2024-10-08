46b8bcaa679ac8501a3b5aaf4d85da77e852e14e
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:27:07 +0300

    converted colors words to the new form

--- src/utils/constants/wordBank/colors.ts
+++ src/utils/constants/wordBank/colors.ts
@@ -2,11 +2,9 @@ import { Category, type Word } from '@src/utils/types';
 
 export const colors: Array<Word> = [
   {
-    main: {
-      spelling: 'красный',
-      soundsLike: '**KRA**SNII',
-      meaning: ['red'],
-    },
+    spelling: 'красный',
+    soundsLike: '**KRA**SNII',
+    meaning: ['red'],
     points: 0,
     categories: [Category.Colors],
   },
