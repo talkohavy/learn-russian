0cad0301bfac1c128b0e1343d983704fb0c12b60
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:30:11 +0300

    converted family words to the new form

--- src/utils/constants/wordBank/family.ts
+++ src/utils/constants/wordBank/family.ts
@@ -1,44 +1,40 @@
-import { Category } from '@src/utils/types';
+import { Category, type Word } from '@src/utils/types';
 
-export const family = [
+export const family: Array<Word> = [
   {
-    main: {
-      spelling: 'мама',
-      soundsLike: 'MAMA',
-      meaning: ['mom'],
-    },
+    spelling: 'мама',
+    soundsLike: 'MAMA',
+    meaning: ['mom'],
     points: 0,
     categories: [Category.Family],
   },
   {
-    main: {
-      spelling: 'папа',
-      soundsLike: 'PAPA',
-      meaning: ['dad'],
-    },
+    spelling: 'папа',
+    soundsLike: 'PAPA',
+    meaning: ['dad'],
     points: 0,
     categories: [Category.Family],
   },
   {
-    main: {
-      spelling: 'брат',
-      soundsLike: 'BRAT',
-      meaning: ['brother'],
-    },
+    spelling: 'брат',
+    soundsLike: 'BRAT',
+    meaning: ['brother'],
     points: 0,
     categories: [Category.Family],
   },
   {
-    main: {
-      spelling: 'сестра',
-      soundsLike: 'SESTRA',
-      meaning: ['sister'],
-    },
-    plural: {
-      spelling: 'сёстры',
-      soundsLike: 'SIYOSTRI',
-      meaning: ['sisters'],
-    },
+    spelling: 'сестра',
+    soundsLike: 'SESTRA',
+    meaning: ['sister'],
+    pluralForeignKey: 'сёстры',
+    points: 0,
+    categories: [Category.Family],
+  },
+  {
+    spelling: 'сёстры',
+    soundsLike: 'SIYOSTRI',
+    meaning: ['sisters'],
+    singularForeignKey: 'сестра',
     points: 0,
     categories: [Category.Family],
   },
