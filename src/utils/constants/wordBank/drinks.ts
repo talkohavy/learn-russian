6a465633ea91214dc36e785570b4246a0fadf791
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:28:22 +0300

    converted drinks words to the new form

--- src/utils/constants/wordBank/drinks.ts
+++ src/utils/constants/wordBank/drinks.ts
@@ -2,47 +2,37 @@ import { Category } from '@src/utils/types';
 
 export const drinks = [
   {
-    main: {
-      spelling: 'чай',
-      soundsLike: 'CHAI',
-      meaning: ['tea'],
-    },
+    spelling: 'чай',
+    soundsLike: 'CHAI',
+    meaning: ['tea'],
     points: 0,
     categories: [Category.Drinks],
   },
   {
-    main: {
-      spelling: 'кофе',
-      soundsLike: '**KO**FИ',
-      meaning: ['coffee'],
-    },
+    spelling: 'кофе',
+    soundsLike: '**KO**FИ',
+    meaning: ['coffee'],
     points: 0,
     categories: [Category.Drinks],
   },
   {
-    main: {
-      spelling: 'сок',
-      soundsLike: 'SOK',
-      meaning: ['juice'],
-    },
+    spelling: 'сок',
+    soundsLike: 'SOK',
+    meaning: ['juice'],
     points: 0,
     categories: [Category.Drinks],
   },
   {
-    main: {
-      spelling: 'вино',
-      soundsLike: 'VI**NO**',
-      meaning: ['wine'],
-    },
+    spelling: 'вино',
+    soundsLike: 'VI**NO**',
+    meaning: ['wine'],
     points: 0,
     categories: [Category.Drinks],
   },
   {
-    main: {
-      spelling: 'вода',
-      soundsLike: 'VADA',
-      meaning: ['water'],
-    },
+    spelling: 'вода',
+    soundsLike: 'VADA',
+    meaning: ['water'],
     points: 0,
     categories: [Category.Drinks, Category.Other],
   },
