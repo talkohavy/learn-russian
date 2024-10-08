27d015b95a3d3edcc675f96c466eb7f8739ac2a4
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:26:05 +0300

    converted bodyRelation words to the new form

--- src/utils/constants/wordBank/bodyRelation.ts
+++ src/utils/constants/wordBank/bodyRelation.ts
@@ -2,65 +2,51 @@ import { Category } from '@src/utils/types';
 
 export const bodyRelation = [
   {
-    main: {
-      spelling: 'я',
-      soundsLike: 'YA',
-      meaning: ['I'],
-    },
+    spelling: 'я',
+    soundsLike: 'YA',
+    meaning: ['I'],
     points: 0,
     categories: [Category.BodyRelation],
   },
   {
-    main: {
-      spelling: 'он',
-      soundsLike: 'ON',
-      meaning: ['he'],
-    },
+    spelling: 'он',
+    soundsLike: 'ON',
+    meaning: ['he'],
     points: 0,
     categories: [Category.BodyRelation],
   },
   {
-    main: {
-      spelling: 'она',
-      soundsLike: 'A**NA**',
-      meaning: ['she'],
-    },
+    spelling: 'она',
+    soundsLike: 'A**NA**',
+    meaning: ['she'],
     points: 0,
     categories: [Category.BodyRelation],
   },
   {
-    main: {
-      spelling: 'мы',
-      soundsLike: 'MUI',
-      meaning: ['we'],
-    },
+    spelling: 'мы',
+    soundsLike: 'MUI',
+    meaning: ['we'],
     points: 0,
     categories: [Category.BodyRelation],
   },
   {
-    main: {
-      spelling: 'они',
-      soundsLike: 'ANI',
-      meaning: ['they'],
-    },
+    spelling: 'они',
+    soundsLike: 'ANI',
+    meaning: ['they'],
     points: 0,
     categories: [Category.BodyRelation],
   },
   {
-    main: {
-      spelling: 'ты',
-      soundsLike: 'TI',
-      meaning: ['you (single)'],
-    },
+    spelling: 'ты',
+    soundsLike: 'TI',
+    meaning: ['you (single)'],
     points: 0,
     categories: [Category.BodyRelation],
   },
   {
-    main: {
-      spelling: 'вы',
-      soundsLike: 'VUI',
-      meaning: ['you (plural)'],
-    },
+    spelling: 'вы',
+    soundsLike: 'VUI',
+    meaning: ['you (plural)'],
     points: 0,
     categories: [Category.BodyRelation],
   },
