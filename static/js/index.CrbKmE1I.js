fd04ec730d52d3607a50eb4d24f0a235c61a9682
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:58:59 +0300

    RELEASE-8

---
+++ static/js/index.CrbKmE1I.js
@@ -0,0 +1,2 @@
+import{a as t,r as a,B as o}from"../../main/index-DnoMDbMV.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=index.RX4t-yFD.js.map
--- static/js/types.CasiXT6B.js
+++ static/js/types.CasiXT6B.js
@@ -1,2 +1,2 @@
 var n=(s=>(s.Animals="animals",s.Jobs="jobs",s.Food="food",s.Drinks="drinks",s.Colors="colors",s.BodyParts="body-parts",s.BodyRelation="body-relation",s.MusicalInstrument="musical-instrument",s.Verbs="verbs",s.Belonging="belonging",s.Family="family",s.QuestionWord="question-word",s.Countries="countries",s.Greetings="greetings",s.Other="other",s))(n||{});export{n as C};
-//# sourceMappingURL=types.Bkc-PTfF.js.map
+//# sourceMappingURL=types.B7F8eDBb.js.map
