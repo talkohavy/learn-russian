a036bc63673431a8a515bdf36085d6ec2d112c68
Tal Kohavy <talkohavy@gmail.com>
2024-10-08 23:35:58 +0300

    converted jobs words to the new form

--- src/utils/constants/wordBank/jobs.ts
+++ src/utils/constants/wordBank/jobs.ts
@@ -1,107 +1,89 @@
-import { Category } from '@src/utils/types';
+import { Category, type Word } from '@src/utils/types';
 
-export const jobs = [
+export const jobs: Array<Word> = [
   {
-    main: {
-      spelling: 'актёр',
-      soundsLike: 'AKTIOR',
-      meaning: ['actor'],
-    },
-    plural: {
-      spelling: 'актёры',
-      soundsLike: 'AKTYORI',
-      meaning: ['actors'],
-    },
+    spelling: 'актёр',
+    soundsLike: 'AKTIOR',
+    meaning: ['actor'],
+    pluralForeignKey: 'актёры',
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'тренер',
-      soundsLike: '**TRAI**NER',
-      meaning: ['coach'],
-    },
+    spelling: 'актёры',
+    soundsLike: 'AKTYORI',
+    meaning: ['actors'],
+    singularForeignKey: 'актёр',
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'капитан',
-      soundsLike: '**KA**PITAN',
-      meaning: ['captain'],
-    },
+    spelling: 'тренер',
+    soundsLike: '**TRAI**NER',
+    meaning: ['coach'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'повар',
-      soundsLike: '**PO**VAR',
-      meaning: ['cook'],
-    },
+    spelling: 'капитан',
+    soundsLike: '**KA**PITAN',
+    meaning: ['captain'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'доктор',
-      soundsLike: '**DOK**TOR',
-      meaning: ['doctor'],
-    },
+    spelling: 'повар',
+    soundsLike: '**PO**VAR',
+    meaning: ['cook'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'инженер',
-      soundsLike: '**EN**ZHINIR',
-      meaning: ['engineer'],
-    },
+    spelling: 'доктор',
+    soundsLike: '**DOK**TOR',
+    meaning: ['doctor'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'учитель',
-      soundsLike: "U**CH'I**TYEL",
-      meaning: ['teacher'],
-    },
+    spelling: 'инженер',
+    soundsLike: '**EN**ZHINIR',
+    meaning: ['engineer'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'психолог',
-      soundsLike: 'PSI**HO**LOG',
-      meaning: ['psychologist'],
-    },
+    spelling: 'учитель',
+    soundsLike: "U**CH'I**TYEL",
+    meaning: ['teacher'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'политик',
-      soundsLike: 'PA**LI**TIC',
-      meaning: ['politician'],
-    },
+    spelling: 'психолог',
+    soundsLike: 'PSI**HO**LOG',
+    meaning: ['psychologist'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'музыкант',
-      soundsLike: 'MOOZI**KANT**',
-      meaning: ['musician'],
-    },
+    spelling: 'политик',
+    soundsLike: 'PA**LI**TIC',
+    meaning: ['politician'],
     points: 0,
     categories: [Category.Jobs],
   },
   {
-    main: {
-      spelling: 'пилот',
-      soundsLike: 'PI**LOT**',
-      meaning: ['pilot'],
-    },
+    spelling: 'музыкант',
+    soundsLike: 'MOOZI**KANT**',
+    meaning: ['musician'],
+    points: 0,
+    categories: [Category.Jobs],
+  },
+  {
+    spelling: 'пилот',
+    soundsLike: 'PI**LOT**',
+    meaning: ['pilot'],
     points: 0,
     categories: [Category.Jobs],
   },
