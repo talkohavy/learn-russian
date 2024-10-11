2d19e00a163a8a93129535c44c341c2c7512398c
Tal Kohavy <talkohavy@gmail.com>
2024-10-11 08:39:12 +0300

    RELEASE-10

---
+++ static/js/index.BsMlxqP1.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-lx7SLu_J.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=index.BT1ggAbq.js.map
--- static/js/index.CrbKmE1I.js
+++
@@ -1,2 +0,0 @@
-import{a as t,r as a,B as o}from"../../main/index-DnoMDbMV.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
-//# sourceMappingURL=index.RX4t-yFD.js.map
