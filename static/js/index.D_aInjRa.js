4e6b43d3a23896f2a2e3ac6de818fb051254f1b7
Tal Kohavy <talkohavy@gmail.com>
2024-10-11 12:20:04 +0300

    RELEASE-11

---
+++ static/js/index.D_aInjRa.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-BM85O6dS.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=index.COWguhON.js.map
