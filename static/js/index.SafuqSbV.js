ff002edd748be9f6cd776f4a69c62e851d609a72
Tal Kohavy <talkohavy@gmail.com>
2024-10-13 10:18:04 +0300

    RELEASE-15

---
+++ static/js/index.SafuqSbV.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-WM5f9z4P.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=index.COWguhON.js.map
