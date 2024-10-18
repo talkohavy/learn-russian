bb04fa5ced10ae5aa7c1daf97210b338cc0f3e28
Tal Kohavy <talkohavy@gmail.com>
2024-10-18 14:38:24 +0300

    RELEASE-19

--- static/js/index.Ks6p6UBO.js
+++
@@ -1,2 +0,0 @@
-import{u as t,r as a,B as o}from"../../main/index-D3aMvSv4.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
-//# sourceMappingURL=index.CrC687_g.js.map
