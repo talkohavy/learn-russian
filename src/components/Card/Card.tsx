61af9e89f63e081743263b88c372a205b777ac0c
Tal Kohavy <talkohavy@gmail.com>
2024-10-18 14:28:05 +0300

    card are now in grid

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ export default function Card(props: CardProps) {
   const { title } = props;
 
   return (
-    <div className='flex size-40 cursor-pointer items-center justify-center rounded-md bg-red-50 hover:bg-red-100 active:bg-red-200 dark:bg-slate-900'>
+    <div className='flex cursor-pointer items-center justify-center rounded-md bg-red-50 hover:bg-red-100 active:bg-red-200 dark:bg-slate-900'>
       <h2 className='font-bold'>{title}</h2>
     </div>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,21 @@
-import { useMemo } from 'react';
+import { useMemo, useRef } from 'react';
+import clsx from 'clsx';
 import Card from '@src/components/Card';
+import { useWidthHeightResize } from '@src/hooks/useWidthHeightResize';
 import { Category } from '@src/utils/types';
+
+enum Sizes {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
+
+function getObjectSize(width: number) {
+  if (width < 600) return Sizes.Small;
+  if (width < 1080) return Sizes.Medium;
+
+  return Sizes.Large;
+}
 
 export default function HomePage() {
   const categories = useMemo(() => {
@@ -9,12 +24,24 @@ export default function HomePage() {
     return keys;
   }, []);
 
+  const elementToWatch = useRef(null);
+
+  const { width } = useWidthHeightResize({ wrapperRef: elementToWatch, ms: 200 });
+
+  const objectSize = getObjectSize(width);
+
   return (
-    <div className='size-full p-6'>
-      <div className='flex flex-wrap justify-start gap-3'>
-        {categories.map((categoryName) => (
-          <Card key={categoryName} title={categoryName} />
-        ))}
+    <div className='size-full overflow-auto p-6'>
+      <div
+        className={clsx(
+          'grid auto-rows-[200px] gap-x-5 gap-y-4',
+          objectSize === Sizes.Small && 'grid-cols-1',
+          objectSize === Sizes.Medium && 'grid-cols-2',
+          objectSize === Sizes.Large && 'grid-cols-4',
+        )}
+        ref={elementToWatch}
+      >
+        {!!width && categories.map((categoryName) => <Card key={categoryName} title={categoryName} />)}
       </div>
     </div>
   );
