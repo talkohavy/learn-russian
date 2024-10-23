818c50ee0240cfa78bec02871415ca460ccc4b62
Tal Kohavy <talkohavy@gmail.com>
2024-10-23 18:56:56 +0300

    RELEASE-21

---
+++ static/js/index.rmAC3-JK.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-DopyNR0x.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=index.RlWxaP2R.js.map
