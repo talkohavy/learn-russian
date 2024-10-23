cd9893baed7cdb0525d8816e9dda8493293a61b3
Tal Kohavy <talkohavy@gmail.com>
2024-10-23 19:25:59 +0300

    HOTFIX: fixed deploy script to include css files

---
+++ static/js/index.YAf-zdX1.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-BzaLtS6B.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=index.RlWxaP2R.js.map
