2e1fce2e300f06c68df0ab12503ffb863c78524d
Tal Kohavy <talkohavy@gmail.com>
2024-10-25 22:16:28 +0300

    RELEASE-22

--- static/js/index.YAf-zdX1.js
+++
@@ -1,2 +0,0 @@
-import{u as t,r as a,B as o}from"../../main/index-BzaLtS6B.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
-//# sourceMappingURL=index.RlWxaP2R.js.map
