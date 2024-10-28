c569ed0539f2f86e0ef5cfe05a0e656b4fb8887c
Tal Kohavy <talkohavy@gmail.com>
2024-10-28 23:47:27 +0200

    RELEASE-23

---
+++ static/js/DisappearingMessage.uD2J64Is.js
@@ -0,0 +1,2 @@
+import{r as t,j as c,c as u}from"../../main/index-BH8JyhBl.js";function f(r){const{value:e,children:i,className:a}=r,[l,s]=t.useState(!1);return t.useEffect(()=>{if(e===null)return;s(!0);const n=setTimeout(()=>s(!1),3e3);return()=>clearTimeout(n)},[e]),c.jsx("div",{className:u("h-10 w-full shrink-0",a),children:l&&i})}export{f as D};
+//# sourceMappingURL=DisappearingMessage.DXP9grwJ.js.map
---
+++ static/js/index.CkvUIAem.js
@@ -0,0 +1,2 @@
+import{u as t,r as a,B as o}from"../../main/index-BH8JyhBl.js";function s(){const e=t();return a.useEffect(()=>{e(o)},[e]),null}export{s as default};
+//# sourceMappingURL=index.CrC687_g.js.map
